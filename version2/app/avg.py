3fffad48d02350d0054c6cb2fb2494aa13442ee5
camaravadi <camaravadi19@gmail.com>
2025-11-26 16:45:48 -0500

    updated avg.py 11/26

--- version2/app/avg.py
+++ version2/app/avg.py
@@ -12,4 +12,4 @@ average = total / len(numbers)
 # Print the result
 print("My numbers are:", numbers)
 print("My average is:", average)
-print("The economy is picking up")
+print("Bitcoin is up and so is ETH")
